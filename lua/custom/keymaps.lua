7b89cf6d27d93fa7311f555d2aa0f5442a82e02c
Agustín Morantes <agustinmorantes@gmail.com>
2024-09-30 20:25:52 -0300

    Added custom keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,8 @@
 -- Shift enter and ctrl enter
 vim.keymap.set('n', '<S-CR>', 'o<Esc>')
 vim.keymap.set('n', '<C-CR>', 'O<Esc>')
+
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==') -- move line up(n)
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==') -- move line down(n)
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv") -- move line down(v)
