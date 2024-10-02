bab389a8afabe2bbac0e12f0391a7277175da83a
Agustín Morantes <agustinmorantes@gmail.com>
2024-10-02 18:43:59 -0300

    Added WSL-specific config

---
+++ lua/custom/wslconfig.lua
@@ -0,0 +1,22 @@
+local uname = vim.uv.os_uname()
+
+_G.OS = uname.sysname
+_G.IS_MAC = OS == 'Darwin'
+_G.IS_LINUX = OS == 'Linux'
+_G.IS_WINDOWS = OS:find 'Windows' and true or false
+_G.IS_WSL = IS_LINUX and uname.release:lower():find 'microsoft' and true or false
+
+if IS_WSL then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
