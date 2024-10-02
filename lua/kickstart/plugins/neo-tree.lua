6a962431f92e75f1f9ef628ed0b230a8803f87b4
Agustín Morantes <agustinmorantes@gmail.com>
2024-10-02 18:06:26 -0300

    Update plugin config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -53,5 +53,13 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
   },
 }
